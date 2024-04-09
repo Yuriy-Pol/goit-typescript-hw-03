e56c801a593c9e7f0169a6d9ac444f9b57e02729
Yuriy Polozhevets <polozhevetsyura@gmail.com>
2024-04-09 21:20:00 +0300

    add

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  private signature: number = Math.random();
+  constructor(signature: number = 0) {
+    this.signature = signature !== 0 ? signature : Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  tenants: Person[] = [];
+  constructor(key: Key) {}
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  openDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  openDoor(givenKey: Key): void {
+    if (givenKey.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+// Сценарій:
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
